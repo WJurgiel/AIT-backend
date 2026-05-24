b1b32385636c27c6d535ec116f873b6b2aa7b791
ajasielec <aniajasielec@gmail.com>
2026-05-24 20:26:10 +0200

    [DOCUMENTATION] fix tests

--- src/test/java/com/ait/aitbackend/user/controller/UserProfileControllerTest.java
+++ src/test/java/com/ait/aitbackend/user/controller/UserProfileControllerTest.java
@@ -1,7 +1,6 @@
 package com.ait.aitbackend.user.controller;
 
 import com.ait.aitbackend.security.JwtService;
-import com.ait.aitbackend.user.entity.UserProfile;
 import com.ait.aitbackend.user.service.UserProfileService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +9,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
-
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserProfileController.class)
@@ -42,32 +35,6 @@ public class UserProfileControllerTest {
     private final String email2 = "other@mail.com";
     private final String password1 = "mockpassword1234!";
 
-
-    @Test
-    void shouldReturn404WhenUserNotFound() throws Exception
-    {
-        // GIVEN
-        when(userService.getUserByUsername("Unknown")).thenReturn(Optional.empty());
-
-        // WHEN & THEN
-        mockMvc.perform(get("/api/users/Unknown"))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    void shouldReturn200AndListOfUsersWhenGettingAllUsers() throws Exception
-    {
-        List<UserProfile> existingUsers = new LinkedList<UserProfile>();
-        existingUsers.add(new UserProfile(username1, email1, password1));
-        existingUsers.add(new UserProfile(username2, email2, password1));
-
-        when(userService.getAllUsers()).thenReturn(existingUsers);
-
-        mockMvc.perform(get("/api/users"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$.size()").value(2));
-    }
 
     @Test
     void shouldAddFavoriteUsingQueryParam() throws Exception {
